5a16c17935440fef5902dbf9ba74adacc40cf88c
Maruf Hasan <marufhasan.connect@gmail.com>
2026-08-17 22:58:04 +0600

    build.sh added...

---
+++ codes/build.sh
@@ -0,0 +1,37 @@
+AVR_MCU="atmega32"
+AVR_PROG="usbasp"
+
+
+avrbuild() {
+  if [ $# -lt 1 ]; then
+    echo "Usage: avrbuild <file.c> [file2.c ...]"
+    return 1
+  fi
+
+  local src="$1"
+  local name="${src##*/}"
+  name="${name%.c}"
+
+  mkdir -p out
+
+  avr-gcc \
+    -mmcu="$AVR_MCU" \
+    -DF_CPU=10000000UL \
+    -Os \
+    -Wall \
+    "$@" \
+    -lm \
+    -o "out/$name.elf" || return 1
+
+  avr-objcopy \
+    -O ihex \
+    -R .eeprom \
+    "out/$name.elf" \
+    "out/$name.hex" || return 1
+
+  rm -f "out/$name.elf"
+
+  echo "✓ Built out/$name.hex"
+}
+
+avrbuild $@
